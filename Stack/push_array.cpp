18d61a16c46fe174de46b6ede19121eb1c121967
Daksh Karangiya <dakshk5610@gmail.com>
2025-07-21 21:21:23 +0530

    Add files via upload

---
+++ Stack/push_array.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+const int MAX = 100; // define max size of the stack
+int top = -1; // global variable to keep track of top index
+
+void push(int s[], int data) {
+    if (top == MAX - 1) {
+        cout << "Stack overflow" << endl;
+    } else {
+        top++;
+        s[top] = data;
+    }
+}
+
+void printStack(int s[]) {
+    if (top == -1) {
+        cout << "Stack is empty" << endl;
+    } else {
+        cout << "Stack elements: ";
+        for (int i = 0; i <= top; i++) {
+            cout << s[i] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    int stack[MAX];
+
+    push(stack, 10);
+    push(stack, 20);
+    push(stack, 30);
+
+    printStack(stack);
+
+    return 0;
+}
